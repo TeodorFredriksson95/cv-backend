9ece13b8ccfcd2e0b5fc3a1cbc5cb1ff76f38ebc
Teodor Fredriksson <teo95@hotmail.se>
2024-04-01 22:19:35 +0200

    sadfasgf

--- CV.Api/Program.cs
+++ CV.Api/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddApplication();
 builder.Services.AddRateLimiting(builder.Configuration);
 
-var connectionString = Environment.GetEnvironmentVariable("POSTGRESQLCONNSTR_DefaultConnection");
+var connectionString = Environment.GetEnvironmentVariable("CUSTOMCONNSTR_DefaultConnection");
 builder.Services.AddDatabase(connectionString);
 
 
