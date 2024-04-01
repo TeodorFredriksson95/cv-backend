18cd1e265ece0a0c52d89f37961d5fc45782b0a5
Teodor Fredriksson <teo95@hotmail.se>
2024-04-01 23:27:14 +0200

    asdgff

--- CV.Api/Program.cs
+++ CV.Api/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddApplication();
 builder.Services.AddRateLimiting(builder.Configuration);
 
-var connectionString = Environment.GetEnvironmentVariable("CUSTOMCONNSTR_DefaultConnection");
+var connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
 builder.Services.AddDatabase(connectionString);
 Console.WriteLine("connection string" + connectionString);
 System.Diagnostics.Trace.WriteLine("connection string" + connectionString);
