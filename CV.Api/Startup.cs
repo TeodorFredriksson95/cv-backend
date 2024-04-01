639ee39c614298d551822231f6bc892343e645b8
Teodor Fredriksson <teo95@hotmail.se>
2024-04-01 16:29:49 +0200

    change startup

--- CV.Api/Startup.cs
+++ CV.Api/Startup.cs
@@ -1,51 +1,51 @@
-﻿using Microsoft.OpenApi.Models;
+﻿//using Microsoft.OpenApi.Models;
 
-namespace CV_backend
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+//namespace CV_backend
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
 
-        public IConfiguration Configuration { get; }
+//        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API Name", Version = "v1" });
-            });
-            // Other service configurations...
-        }
+//        // This method gets called by the runtime. Use this method to add services to the container.
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            services.AddControllers();
+//            services.AddSwaggerGen(c =>
+//            {
+//                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API Name", Version = "v1" });
+//            });
+//            // Other service configurations...
+//        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Unidevweb API v1");
-                });
-            }
+//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//                app.UseSwagger();
+//                app.UseSwaggerUI(c =>
+//                {
+//                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Unidevweb API v1");
+//                });
+//            }
 
-            app.UseHttpsRedirection();
+//            app.UseHttpsRedirection();
 
-            app.UseRouting();
+//            app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+//            app.UseAuthentication();
+//            app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
-}
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapControllers();
+//            });
+//        }
+//    }
+//}
