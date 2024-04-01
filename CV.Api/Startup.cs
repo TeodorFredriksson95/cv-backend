b7fa25868227b05abbb6497473e7884254b937f7
Teodor Fredriksson <teo95@hotmail.se>
2024-04-01 14:04:00 +0200

    a

---
+++ CV.Api/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace CV_backend
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Your services configuration here
+            // For Swagger generation, it might not be necessary to duplicate all services.
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Your middleware configuration here
+            // For Swagger generation, it might not be necessary to configure middleware.
+        }
+    }
+}
