6124d163f020118474a05a1d961557eab759eac7
Teodor Fredriksson <teo95@hotmail.se>
2024-04-01 19:08:07 +0200

    wfe15

---
+++ CV.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CV_backend.Controllers
+{
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Route("api/test")]
+        public IActionResult Index()
+        {
+            return Ok(new {message = "test controller went through"});
+        }
+    }
+}
