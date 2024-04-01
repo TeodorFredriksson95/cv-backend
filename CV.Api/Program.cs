1aef4ce7464935a505dbe50101a6f6bce3f6a3e4
Teodor Fredriksson <teo95@hotmail.se>
2024-04-02 00:12:15 +0200

    kjbnbn

--- CV.Api/Program.cs
+++ CV.Api/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddApplication();
 builder.Services.AddRateLimiting(builder.Configuration);
 
-var connectionString = Environment.GetEnvironmentVariable("unidevwebcon");
+var connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
 builder.Services.AddDatabase(connectionString);
 Console.WriteLine("connection string" + connectionString);
 System.Diagnostics.Trace.WriteLine("connection string" + connectionString);
